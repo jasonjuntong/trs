9f924f5f44585ecf5487a8651d1720cd3b7d3679
jasonjuntong <jjasonjuntong@gmail.com>
2025-03-02 16:44:40 +0800

    re-setup project with vue and primevuew

---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<script setup lang='ts'>
+import Button from "primevue/button"
+
+const onClickHandler = () => console.log("clicked")
+</script>
+
+<template>
+  <div>
+    <Button label="Verify" @click="onClickHandler" />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
